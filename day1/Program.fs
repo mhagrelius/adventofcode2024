f763f3de3aa6a27e32fa144911becd87ebf30b39
Matthew Hagrelius <matthew@hagreli.us>
2024-12-01 18:13:09 -0500

    Refactor for improved array operations
    Streamlined array operations for cleaner and more efficient code by removing redundant variable assignments and simplifying function calls. Enhanced readability by using concise and direct expressions. These changes aim to improve maintainability and performance without altering the functionality.

--- day1/Program.fs
+++ day1/Program.fs
@@ -1,36 +1,26 @@
-﻿open System
-open System.IO
+﻿open System.IO
 
 let filePath = "input.txt"
 let fileContent = File.ReadAllLines(filePath)
 
 let parseLine (line: string)=
     let parts = line.Split(' ') |> Array.filter (fun s -> s <> "")
-    let firstValue = parts.[0].Trim() |> int
-    let secondValue = parts.[1].Trim() |> int
+    let firstValue = parts[0].Trim() |> int
+    let secondValue = parts[1].Trim() |> int
     firstValue, secondValue
-    
-    
+       
 let sortArrays (firstArray, secondArray) =
-    let firstSortedArray = firstArray |> Array.sort
-    let secondSortedArray = secondArray |> Array.sort
-    (firstSortedArray, secondSortedArray)
+    Array.sort firstArray, Array.sort secondArray
     
-let calcDistances arrays =
-    let (firstSortedArray, secondSortedArray) = arrays
-    firstSortedArray
-    |> Array.map2 (fun x y -> abs (x - y)) secondSortedArray
+let calcDistances (firstSortedArray, secondSortedArray) =
+    Array.map2 (fun x y -> abs (x - y)) firstSortedArray secondSortedArray
     |> Array.sum
     
 let occurrences value array =
-    array
-    |> Array.where (fun x -> x = value)
-    |> Array.length
+    Array.filter ((=) value) array |> Array.length
     
-let calcSimilarities arrays =
-    let (firstSortedArray, secondSortedArray) = arrays
-    firstSortedArray
-    |> Array.sumBy (fun x -> x * (occurrences x secondSortedArray))
+let calcSimilarities (firstSortedArray, secondSortedArray) =
+    Array.sumBy (fun x -> x * occurrences x secondSortedArray) firstSortedArray
                   
     
 let data = 
