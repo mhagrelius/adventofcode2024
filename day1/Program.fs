87beb7028a6854975d715ed83067751a95f26218
Matthew Hagrelius <matthew@hagreli.us>
2024-12-01 20:34:19 -0500

    refactor composition and legibility

--- day1/Program.fs
+++ day1/Program.fs
@@ -7,28 +7,32 @@ let parseLine (line: string)=
     let parts = line.Split(' ') |> Array.filter (fun s -> s <> "")
     let firstValue = parts[0].Trim() |> int
     let secondValue = parts[1].Trim() |> int
-    firstValue, secondValue
+    (firstValue, secondValue)
        
 let sortArrays (firstArray, secondArray) =
-    Array.sort firstArray, Array.sort secondArray
-    
+    (Array.sort firstArray, Array.sort secondArray)
+
 let calcDistances (firstSortedArray, secondSortedArray) =
-    Array.map2 (fun x y -> abs (x - y)) firstSortedArray secondSortedArray
+    secondSortedArray
+    |> Array.map2 (fun x y -> abs (x - y)) firstSortedArray
     |> Array.sum
     
 let occurrences value array =
-    Array.filter ((=) value) array |> Array.length
+    Array.filter ((=) value) array
+    |> Array.length
     
 let calcSimilarities (firstSortedArray, secondSortedArray) =
-    Array.sumBy (fun x -> x * occurrences x secondSortedArray) firstSortedArray
-                  
-    
+    firstSortedArray
+    |> Array.sumBy (fun x -> x * occurrences x secondSortedArray)
+
+let sortThenCalcDistances = sortArrays >> calcDistances                  
+let sortThenCalcSimilarities = sortArrays >> calcSimilarities 
+
 let data = 
     fileContent 
     |> Array.map parseLine
     |> Array.unzip
-    |> sortArrays
-    |> calcSimilarities // Swap in calcDistances if solving part 1
+    |> sortThenCalcSimilarities // Swap in sortThenCalcDistances if solving part 1
     |> printfn "%d"
 
 
