2b1509459f3a8586c1d2cae4205d21db273b0a5e
Matthew Hagrelius <matthew@hagreli.us>
2024-12-03 10:07:25 -0500

    Added day 3 part 1 solution

---
+++ day3/Program.fs
@@ -0,0 +1,9 @@
+﻿open System.Text.RegularExpressions
+
+let regex = new Regex @"mul\((\d+),(\d+)\)"
+let input = System.IO.File.ReadAllText "input.txt"
+
+regex.Matches input 
+    |> Seq.cast<Match>
+    |> Seq.sumBy (fun m -> int m.Groups[1].Value * int m.Groups[2].Value)
+    |> printfn "%d"
